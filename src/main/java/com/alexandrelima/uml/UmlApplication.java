bd151c56906a427de1cda23c366ff6c9d330d5c5
Alexandre Lima <alexandre87ml@gmail.com>
2020-09-08 08:28:23 -0300

    Projeto criado

---
+++ src/main/java/com/alexandrelima/uml/UmlApplication.java
@@ -0,0 +1,13 @@
+package com.alexandrelima.uml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmlApplication.class, args);
+	}
+
+}
