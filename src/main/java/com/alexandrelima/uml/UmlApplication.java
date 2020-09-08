2bf2246958e89428f08e4ef761ce345e5925f6e5
Alexandre Lima <alexandre87ml@gmail.com>
2020-09-08 17:35:29 -0300

    Classes de categoria e controles para ela

--- src/main/java/com/alexandrelima/uml/UmlApplication.java
+++ src/main/java/com/alexandrelima/uml/UmlApplication.java
@@ -1,13 +1,32 @@
 package com.alexandrelima.uml;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class UmlApplication {
+import com.alexandrelima.uml.domain.Categoria;
+import com.alexandrelima.uml.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class UmlApplication implements CommandLineRunner{
+	
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(UmlApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Categoria 1");
+		Categoria cat2 = new Categoria(null, "Categoria 2");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
