95f17a9791cf91265ea9ba8fc476d846637268dd
Alexandre Lima <alexandre87ml@gmail.com>
2020-09-10 18:23:19 -0300

    retirado JsonManagedReference e substituido JsonBackReference por JsonIgnore

--- src/main/java/com/alexandrelima/uml/domain/Cliente.java
+++ src/main/java/com/alexandrelima/uml/domain/Cliente.java
@@ -15,8 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.alexandrelima.uml.domain.enums.TipoCliente;
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Cliente implements Serializable{
@@ -35,11 +34,10 @@ public class Cliente implements Serializable{
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 
-	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 
-	@JsonBackReference
+	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
